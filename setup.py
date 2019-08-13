9a8ec0f0fa2291cd266e4ea63787343b18f68e11
bartlomiej <bnowak@i2asolutions.com>
2019-08-13 15:09:02 +0200

    changes version to 0.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="django-db-views",
-    version="0.0.3",
+    version="0.0.4",
     description="Handle database views. "
                 "Allow to create views migrations. "
                 "View migrations using django code. "
