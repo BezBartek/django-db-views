ed026b2fb27a0a9ac0ac583f01c52f2ac2cd280c
Dmitry Shachnev <mitya57@gmail.com>
2019-11-05 16:37:09 +0300

    Add SQLite support

--- django_db_views/migration_functions.py
+++ django_db_views/migration_functions.py
@@ -13,20 +13,14 @@ class ForwardViewMigration(ViewMigration):
 
     def __call__(self, apps, schema_editor):
         if self.view_definition:
-            schema_editor.execute(
-                """CREATE OR REPLACE VIEW %s AS %s;""" % (self.table_name, self.view_definition)
-            )
+            schema_editor.execute("DROP VIEW IF EXISTS %s;" % self.table_name)
+            schema_editor.execute("CREATE VIEW %s as %s;" % (self.table_name, self.view_definition))
 
 
 @deconstructible
 class BackwardViewMigration(ViewMigration):
 
     def __call__(self, apps, schema_editor):
+        schema_editor.execute("DROP VIEW IF EXISTS %s;" % self.table_name)
         if self.view_definition:
-            schema_editor.execute(
-                """CREATE OR REPLACE VIEW %s AS %s;""" % (self.table_name, self.view_definition)
-            )
-        else:
-            schema_editor.execute(
-                'DROP VIEW IF EXISTS %s;' % self.table_name
-            )
+            schema_editor.execute("CREATE VIEW %s as %s;" % (self.table_name, self.view_definition))
